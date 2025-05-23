2c5375f42fed7d40f70d75adf42cbf413fe0762c
Xin Yao <90070559+XYao2021@users.noreply.github.com>
2025-05-23 12:27:14 -0400

    Update CIFAR10Model.py

--- model/CIFAR10Model.py
+++ model/CIFAR10Model.py
@@ -64,10 +64,10 @@ class CIFAR10Model(nn.Module):
         self.fc1.apply(init_weights)
 
     def forward(self, x):
-        print(x.shape)
+        # print(x.shape)
         conv_ = self.conv(x)
-        print(conv_.shape)
-        fc_ = conv_.view(-1, 8 * 8 * 32)
+        # print(conv_.shape)
+        fc_ = conv_.view(x.size(0), -1)
         fc1_ = self.fc1(fc_)
         output = self.fc2(fc1_)
         return output
